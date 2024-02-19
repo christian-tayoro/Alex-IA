e6a88cf6c334e3d9a863c6d068ad6c5ae956c410
CHRISTIAN TAYORO <christian.tayoro@cgi.com>
2024-02-19 10:53:59 -0500

    début de développement du backend

---
+++ backend/AlexIA.Backend/Controllers/CVController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlexIA.Backend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CVController : ControllerBase
+    {
+
+    }
+}
---
+++ backend/AlexIA.Backend/Model/CVModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AlexIA.Backend.Model
+{
+    public class CVModel
+    {
+        public string? id { get; set; }
+        public string? Title { get; set; }
+        public string? UrlStorage { get; set; }
+    }
+}
